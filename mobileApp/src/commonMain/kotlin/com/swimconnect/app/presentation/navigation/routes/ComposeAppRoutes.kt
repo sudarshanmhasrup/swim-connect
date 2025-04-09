7f56f0256f127ecb25265698507fdd03245aa433
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-09 17:54:01 +0530

    Create ComposeAppRoutes to manage navigation routes

---
+++ mobileApp/src/commonMain/kotlin/com/swimconnect/app/presentation/navigation/routes/ComposeAppRoutes.kt
@@ -0,0 +1,6 @@
+package com.swimconnect.mobile.presentation.navigation.routes
+
+object ComposeAppRoutes {
+    const val LANDING_PAGE = "LANDING_PAGE"
+    const val FINISH_SETUP_PAGE = "FINISH_SETUP_PAGE"
+}
