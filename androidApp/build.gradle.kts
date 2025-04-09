f06bdae2563050f6413c879f099d771a9ae9687a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-09 11:08:43 +0530

    Define android app package information

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
 }
+
+android {
+    namespace = libs.versions.android.applicationId.get()
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = libs.versions.android.applicationId.get()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        versionCode = libs.versions.android.versionCode.get().toInt()
+        versionName = libs.versions.android.versionName.get()
+    }
+}
