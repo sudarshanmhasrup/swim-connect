858d78fe876ef8302b058f27465b20e8006ecbe0
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-10 15:46:55 +0530

    Create "child-details" module with Kotlin Multiplatform, Android, and Compose plugins. Add dependencies for ViewModel, Navigation, Compose components, and other projects. Configure the Android namespace, compile SDK, and custom build directory.

---
+++ child-details/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose.multiplatform)
+}
+
+group = libs.versions.childDetails.groupId.get()
+version = libs.versions.childDetails.version.get()
+
+kotlin {
+    androidTarget()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.lifecycle.viewmodel.compose)
+            implementation(libs.navigation.compose)
+            implementation(compose.components.resources)
+            implementation(compose.foundation)
+            implementation(compose.ui)
+            implementation(project(":designSystem"))
+            implementation(project(":shared"))
+        }
+    }
+}
+
+android {
+    namespace = libs.versions.childDetails.groupId.get()
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+}
+
+// Custom build directory
+layout.buildDirectory.set(file("$rootDir/.build/childDetails"))
