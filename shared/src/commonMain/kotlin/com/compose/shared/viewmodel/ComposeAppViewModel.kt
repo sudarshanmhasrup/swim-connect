c3248a3f18c5805b464df600e705f1977db6c4aa
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-10 15:09:41 +0530

    Store and check landing page view status using UserPreferenceManager

--- shared/src/commonMain/kotlin/com/compose/shared/viewmodel/ComposeAppViewModel.kt
+++ shared/src/commonMain/kotlin/com/compose/shared/viewmodel/ComposeAppViewModel.kt
@@ -1,15 +1,23 @@
 package com.compose.shared.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.platform.api.util.UserPreferenceManager
 
-class ComposeAppViewModel: ViewModel() {
-
-    fun checkIfLandingPageIsViewed(): Boolean {
-        return true
-    }
+class ComposeAppViewModel : ViewModel() {
+    private val landingPageViewedStatusPreferenceKey = "landing-page-viewed-status"
 
     fun saveLandingPageAsViewed() {
+        UserPreferenceManager.savePreference(
+            key = landingPageViewedStatusPreferenceKey,
+            value = "viewed"
+        )
+    }
 
+    fun checkIfLandingPageIsViewed(): Boolean {
+        val landingPageViewStatus = UserPreferenceManager.getPreference(
+            key = landingPageViewedStatusPreferenceKey
+        )
+        return landingPageViewStatus != null
     }
 
     fun checkIfChildDetailsPageIsViewed(): Boolean {
