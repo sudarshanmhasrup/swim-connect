2884a498cf571004d50b3bd2789cebc19b43bc9a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-10 15:10:51 +0530

    Create ComposeApp file

---
+++ mobileApp/src/commonMain/kotlin/com/swimconnect/app/ComposeApp.kt
@@ -0,0 +1,22 @@
+package com.swimconnect.app
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.compose.shared.extentions.backgroundContainerModifier
+import com.compose.shared.viewmodel.ComposeAppViewModel
+import com.design.system.api.ComposeAppTheme
+import com.swimconnect.app.presentation.navigation.composeNavigation.ComposeAppNavigation
+import com.swimconnect.app.theme.SwimConnectTheme
+
+@Composable
+fun ComposeApp(isDarkMode: Boolean = false) {
+    val composeAppViewModel: ComposeAppViewModel = viewModel()
+    SwimConnectTheme(isDarkMode = isDarkMode) {
+        val backgroundColor = ComposeAppTheme.colors.backgroundColor
+        Column(modifier = Modifier.backgroundContainerModifier(color = backgroundColor)) {
+            ComposeAppNavigation(composeAppViewModel = composeAppViewModel)
+        }
+    }
+}
