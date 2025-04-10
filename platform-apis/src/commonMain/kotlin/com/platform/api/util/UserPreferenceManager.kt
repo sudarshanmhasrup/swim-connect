4742499da889a2db9e654b142f54268927716998
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-10 15:09:28 +0530

    Implement UserPreferenceManager with savePreference and getPreference functions.

--- platform-apis/src/commonMain/kotlin/com/platform/api/util/UserPreferenceManager.kt
+++ platform-apis/src/commonMain/kotlin/com/platform/api/util/UserPreferenceManager.kt
@@ -1,8 +1,41 @@
 package com.platform.api.util
 
-expect object UserPreferenceManager {
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-    fun savePreference(key: String, value: String)
+object UserPreferenceManager {
+    private var preferences: DataStore<Preferences>? = DataStoreManager.getDataStoreInstance()
 
-    fun getPreference(key: String): String?
+    private fun initialize() {
+        preferences = DataStoreManager.getDataStoreInstance()
+    }
+
+    fun savePreference(key: String, value: String) {
+        val dataStore = preferences ?: return
+        val preferenceKey = stringPreferencesKey(name = key)
+        CoroutineScope(Dispatchers.IO).launch {
+            dataStore.edit {
+                it[preferenceKey] = value
+            }
+        }
+    }
+
+    fun getPreference(key: String): String? {
+        val dataStore = preferences ?: return null
+        val preferenceKey = stringPreferencesKey(name = key)
+        return runBlocking {
+            dataStore.data
+                .map { it[preferenceKey] }
+                .firstOrNull()
+        }
+    }
 }
