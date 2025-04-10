286d4f790724e309513463e7cfa0c90412ae4e62
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-10 15:35:01 +0530

    Initialize DataStoreManager with application context

--- androidApp/src/main/kotlin/com/swimconnect/app/MainActivity.kt
+++ androidApp/src/main/kotlin/com/swimconnect/app/MainActivity.kt
@@ -5,14 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.platform.api.util.DataStoreManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
+        initializeManagers()
         setContent {
             AndroidApp(isDarkMode = false)
         }
+    }
+
+    private fun initializeManagers() {
+        DataStoreManager.initialize(context = this)
     }
 }
