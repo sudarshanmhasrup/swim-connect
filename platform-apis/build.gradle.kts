d46d3d453bd0f70b39f6e09f1756960e78092fa0
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-10 12:59:50 +0530

    Apply "org.jetbrains.kotlin.multiplatform" and "com.android.library" plugins, configure targets and android settings, and set a custom build directory for the platform-apis module.

--- platform-apis/build.gradle.kts
+++ platform-apis/build.gradle.kts
@@ -1,3 +1,23 @@
 plugins {
-
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
 }
+
+group = libs.versions.platformApis.groupId.get()
+version = libs.versions.platformApis.version.get()
+
+kotlin {
+    androidTarget()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+}
+
+android {
+    namespace = libs.versions.platformApis.groupId.get()
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+}
+
+// Custom build directory
+layout.buildDirectory.set(file("$rootDir/.build/platformApis"))
