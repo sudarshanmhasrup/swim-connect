0b3f607e17c77aea3ab7dd81cc967c4c777410f4
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-11 20:19:45 +0530

    Define common, light, and dark color palettes in the theme

--- mobileApp/src/commonMain/kotlin/com/swimconnect/app/theme/Color.kt
+++ mobileApp/src/commonMain/kotlin/com/swimconnect/app/theme/Color.kt
@@ -4,13 +4,22 @@ import androidx.compose.ui.graphics.Color
 
 // Common color
 val primaryColor = Color(0xFF0195E3)
-val primaryDisabledColor = Color(0xFF84BEDD)
+val inputErrorColor = Color(0xFFF86D70)
 
 // Light colors
 val backgroundColorLight = Color(0xFFFFFFFF)
 val secondaryColorLight = Color(0xFFF7F9FF)
+val primaryDisabledColorLight = Color(0xFF84BEDD)
 val primaryTextColorLight = Color(0xFF181C20)
 val secondaryTextColorLight = Color(0xFF42474E)
 val borderColorLight = Color(0xFFF1F4F9)
 val inputHintColorLight = Color(0xFFE0E2E8)
-val inputErrorColorLight = Color(0xFFF86D70)
+
+// Dark colors
+val backgroundColorDark = Color(0xFF101418)
+val secondaryColorDark = Color(0xFF181C20)
+val primaryDisabledColorDark = Color(0xFF3A4857)
+val primaryTextColorDark = Color(0xFFE0E2E8)
+val secondaryTextColorDark = Color(0xFFC2C7CF)
+val borderColorDark = Color(0xFF1C2024)
+val inputHintColorDark = Color(0xFF36393E)
