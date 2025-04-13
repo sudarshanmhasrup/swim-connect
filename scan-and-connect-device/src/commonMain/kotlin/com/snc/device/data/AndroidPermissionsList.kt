072727c801d30bd707439959b9a2b5c052e6bc3c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-13 16:06:03 +0530

    Create `AndroidPermissionsList` for Bluetooth and location permissions
    This commit introduces a new `AndroidPermissionsList.kt` file. This file defines `androidPermissionsList`, a list of `RuntimePermission` objects. This list contains necessary information for Bluetooth (`BLUETOOTH_SCAN`) and location (`LOCATION`) permissions, including icons, labels, descriptions, and whether the permission is granted.

---
+++ scan-and-connect-device/src/commonMain/kotlin/com/snc/device/data/AndroidPermissionsList.kt
@@ -0,0 +1,30 @@
+package com.snc.device.data
+
+import com.snc.device.model.RuntimePermission
+import com.snc.device.scan_and_connect_device.generated.resources.Res
+import com.snc.device.scan_and_connect_device.generated.resources.ic_location
+import com.snc.device.scan_and_connect_device.generated.resources.ic_nearby_devices
+import com.snc.device.scan_and_connect_device.generated.resources.location_permission_description
+import com.snc.device.scan_and_connect_device.generated.resources.location_permission_label
+import com.snc.device.scan_and_connect_device.generated.resources.nearby_devices_permission_description
+import com.snc.device.scan_and_connect_device.generated.resources.nearby_devices_permission_label
+import dev.icerock.moko.permissions.Permission
+import dev.icerock.moko.permissions.bluetooth.BLUETOOTH_SCAN
+import dev.icerock.moko.permissions.location.LOCATION
+
+val androidPermissionsList = listOf(
+    RuntimePermission(
+        image = Res.drawable.ic_nearby_devices,
+        label = Res.string.nearby_devices_permission_label,
+        description = Res.string.nearby_devices_permission_description,
+        isPermissionGranted = false,
+        permission = Permission.BLUETOOTH_SCAN
+    ),
+    RuntimePermission(
+        image = Res.drawable.ic_location,
+        label = Res.string.location_permission_label,
+        description = Res.string.location_permission_description,
+        isPermissionGranted = false,
+        permission = Permission.LOCATION
+    )
+)
