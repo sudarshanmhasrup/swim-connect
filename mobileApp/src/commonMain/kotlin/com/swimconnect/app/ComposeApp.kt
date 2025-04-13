79ca410751a7425ac99181ce50e023a2fc7b825e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-13 11:29:54 +0530

    Add `SystemBarBackground` to `ComposeApp`
    This commit introduces a `SystemBarBackground` composable function to `ComposeApp.kt`. This function renders a background for the system status and navigation bars, enhancing the visual appearance of the app. The `ComposeApp` now uses `SystemBarBackground` to provide a consistent system bars experience.
    It also updates the `ComposeAppNavigation` composable to be placed in a `Box` composable with `Column`.

--- mobileApp/src/commonMain/kotlin/com/swimconnect/app/ComposeApp.kt
+++ mobileApp/src/commonMain/kotlin/com/swimconnect/app/ComposeApp.kt
@@ -1,11 +1,23 @@
 package com.swimconnect.app
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.compose.shared.extentions.backgroundContainerModifier
-import com.compose.shared.viewmodel.ComposeAppViewModel
+import com.compose.shared.presentation.ComposeAppViewModel
 import com.design.system.api.ComposeAppTheme
 import com.swimconnect.app.presentation.navigation.ComposeAppNavigation
 import com.swimconnect.app.theme.SwimConnectTheme
@@ -14,9 +26,49 @@ import com.swimconnect.app.theme.SwimConnectTheme
 fun ComposeApp(isDarkMode: Boolean = false) {
     val composeAppViewModel: ComposeAppViewModel = viewModel()
     SwimConnectTheme(isDarkMode = isDarkMode) {
+        val systemBarBackgroundColor = if (isDarkMode) Color(0xFF101418) else Color(0xFFFFFFFF)
         val backgroundColor = ComposeAppTheme.colors.backgroundColor
-        Column(modifier = Modifier.backgroundContainerModifier(color = backgroundColor)) {
-            ComposeAppNavigation(composeAppViewModel = composeAppViewModel)
+
+        val density = LocalDensity.current
+        val insets = WindowInsets.systemBars
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.backgroundContainerModifier(backgroundColor = backgroundColor)) {
+                ComposeAppNavigation(composeAppViewModel = composeAppViewModel)
+            }
+            // Status bar
+            SystemBarBackground(
+                backgroundColor = systemBarBackgroundColor,
+                height = with(density) { insets.getTop(this).toDp() },
+                contentAlignment = Alignment.TopCenter,
+                modifier = Modifier.fillMaxWidth()
+            )
+            // Navigation bar
+            SystemBarBackground(
+                backgroundColor = systemBarBackgroundColor,
+                height = with(density) { insets.getBottom(this).toDp() },
+                contentAlignment = Alignment.BottomCenter,
+                modifier = Modifier.fillMaxSize()
+            )
         }
+    }
+}
+
+
+@Composable
+private fun SystemBarBackground(
+    backgroundColor: Color,
+    height: Dp,
+    contentAlignment: Alignment,
+    modifier: Modifier = Modifier
+) {
+    Box(contentAlignment = contentAlignment, modifier = modifier) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(height)
+                .alpha(0.80f)
+                .background(color = backgroundColor)
+        )
     }
 }
