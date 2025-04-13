36c9b7b29ae87286e2e65867d6d0205b0d721b45
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-13 16:32:24 +0530

    Add BLUETOOTH_CONNECT permission to AndroidPermissionsList
    This commit adds the `BLUETOOTH_CONNECT` permission to the `AndroidPermissionsList`. `BLUETOOTH_SCAN` and `LOCATION` are marked for UI display while `BLUETOOTH_CONNECT` is added with `showOnUi` set to `false`.

--- scan-and-connect-device/src/commonMain/kotlin/com/snc/device/data/AndroidPermissionsList.kt
+++ scan-and-connect-device/src/commonMain/kotlin/com/snc/device/data/AndroidPermissionsList.kt
@@ -9,6 +9,7 @@ import com.snc.device.scan_and_connect_device.generated.resources.location_permi
 import com.snc.device.scan_and_connect_device.generated.resources.nearby_devices_permission_description
 import com.snc.device.scan_and_connect_device.generated.resources.nearby_devices_permission_label
 import dev.icerock.moko.permissions.Permission
+import dev.icerock.moko.permissions.bluetooth.BLUETOOTH_CONNECT
 import dev.icerock.moko.permissions.bluetooth.BLUETOOTH_SCAN
 import dev.icerock.moko.permissions.location.LOCATION
 
@@ -18,13 +19,20 @@ val androidPermissionsList = listOf(
         label = Res.string.nearby_devices_permission_label,
         description = Res.string.nearby_devices_permission_description,
         isPermissionGranted = false,
-        permission = Permission.BLUETOOTH_SCAN
+        permission = Permission.BLUETOOTH_SCAN,
+        showOnUi = true
+    ),
+    RuntimePermission(
+        isPermissionGranted = false,
+        permission = Permission.BLUETOOTH_CONNECT,
+        showOnUi = false
     ),
     RuntimePermission(
         image = Res.drawable.ic_location,
         label = Res.string.location_permission_label,
         description = Res.string.location_permission_description,
         isPermissionGranted = false,
-        permission = Permission.LOCATION
+        permission = Permission.LOCATION,
+        showOnUi = true
     )
 )
