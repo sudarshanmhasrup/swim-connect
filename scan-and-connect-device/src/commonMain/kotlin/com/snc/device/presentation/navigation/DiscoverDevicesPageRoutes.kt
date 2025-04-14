8c98795fa497981deabe402e61631c0c04e0cb3e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-14 13:43:31 +0530

    Create navigation routes for Discover Devices page
    This commit introduces navigation routes for the Discover Devices page, defining constants for: `MISSING_PERMISSIONS_UI`, `DISCOVER_DEVICES_UI`, `ENABLE_BLUETOOTH_UI`, and `ENABLE_LOCATION_UI`.

---
+++ scan-and-connect-device/src/commonMain/kotlin/com/snc/device/presentation/navigation/DiscoverDevicesPageRoutes.kt
@@ -0,0 +1,8 @@
+package com.snc.device.presentation.navigation
+
+object DiscoverDevicesPageRoutes {
+    const val MISSING_PERMISSIONS_UI = "MissingPermissionsUi"
+    const val DISCOVER_DEVICES_UI = "DiscoverDevicesUi"
+    const val ENABLE_BLUETOOTH_UI = "EnableBluetoothUi"
+    const val ENABLE_LOCATION_UI = "EnableLocationUi"
+}
