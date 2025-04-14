8d153d804c5a6bc7212458c372683a6cc503ca63
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-14 14:14:54 +0530

    Refactor Bluetooth scanning with BlueFalcon
    This commit refactors the Bluetooth scanning process using the `BlueFalcon` library.
    -   Adds `onDiscoveryStarted` to notify when scanning starts.
    -   Adds `onBluetoothDisabledError` and `onLocationDisabledError` callbacks.
    -   Passes the `peripheral` to `SwimConnectDevice`.
    -   Adds `stopScanning` and `connectDevice` functions.
    -   Improves scanning logic to handle multiple devices.
    -   Moves `BluetoothManager` to an object.

--- platform-apis/src/commonMain/kotlin/com/platform/api/util/BluetoothManager.kt
+++ platform-apis/src/commonMain/kotlin/com/platform/api/util/BluetoothManager.kt
@@ -1,32 +1,51 @@
 package com.platform.api.util
 
-import co.touchlab.kermit.Logger
 import com.platform.api.model.SwimConnectDevice
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 object BluetoothManager {
     private val blueFalcon = BlueFalconManager.getBlueFalcon()
 
-    val log = Logger.withTag("MyApp")
-
-    fun startScanning(onDeviceFound: (SwimConnectDevice) -> Unit) {
+    fun startScanning(
+        onDiscoveryStarted: () -> Unit,
+        onDeviceFound: (SwimConnectDevice) -> Unit,
+        onBluetoothDisabledError: () -> Unit,
+        onLocationDisabledError: () -> Unit,
+    ) {
+        // Check if bluetooth is enabled
         blueFalcon.scan()
+
+        // Start discovery
         CoroutineScope(Dispatchers.IO).launch {
-            log.i { "Started coroutine to collect peripherals" }
-            blueFalcon.peripherals.collect { foundDevicesList ->
-                log.i { "Collected a list of ${foundDevicesList.size} devices" }
-                foundDevicesList.forEach { device ->
-                    val name = device.name ?: "Unknown device"
-                    val uuid = device.uuid
-                    val device = SwimConnectDevice(name = name, address = uuid)
-                    log.i { "Found device: $name ($uuid)" }
-                    onDeviceFound(device)
+            blueFalcon.peripherals
+                .onStart {
+                    onDiscoveryStarted()
                 }
-            }
+                .collect { foundDevicesList ->
+                    foundDevicesList.forEach { device ->
+                        val name = device.name ?: "Unknown device"
+                        val uuid = device.uuid
+                        val device = SwimConnectDevice(
+                            name = name,
+                            address = uuid,
+                            peripheral = device
+                        )
+                        onDeviceFound(device)
+                    }
+                }
+
         }
     }
 
+    fun stopScanning() {
+        blueFalcon.stopScanning()
+    }
+    
+    fun connectDevice(device: SwimConnectDevice) {
+        blueFalcon.connect(device.peripheral)
+    }
 }
