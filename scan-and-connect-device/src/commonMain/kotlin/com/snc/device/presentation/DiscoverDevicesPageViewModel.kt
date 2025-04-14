6a258acda295137b7211cc0c4c3251d7881e2df3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-04-14 14:11:02 +0530

    feat: Add DiscoverDevicesPageViewModel for device discovery and permission management
    This commit introduces the `DiscoverDevicesPageViewModel`, which handles the logic for discovering devices and managing permissions.
    
    **Key changes:**
    
    -   **`DiscoverDevicesPageViewModel`**:
        -   Manages the UI state for the device discovery page.
        -   Handles runtime permission checks and requests using `PermissionsController`.
        -   Provides methods to check if all or main permissions are granted (`checkIfAllPermissionsAreGranted`, `areAllPermissionsGranted`, `areMainPermissionsGranted`).
        -   Allows requesting specific runtime permissions (`requestRuntimePermission`).
        -   Allows requesting background permissions (`requestBackgroundPermissions`).
        -   Manages a list of scanned devices (`updateScannedDevicesList`).
        -   Starts and stops the Bluetooth scanning process (`startScanning`, `stopScanning`, `startRescanning`).
        -   Connects to a selected device (`connect`).
        -   Checks if location is enabled or not.
    -   **Initial Permissions:**
        -   On Android, default to granted permissions.
        -   On other platforms, initialize with a list of runtime permissions and check their status.
    -   **Scanning:**
        - Starts scanning for bluetooth devices using the `BluetoothManager`.
        - Stop scanning after 30 seconds.
    -   **Rescanning:**
        - Clears the current devices list before starting new scan.
    -   **Connecting:**
        - Connects to the chosen device.
    -   **Permissions:**
        -   Uses `PermissionsController` to manage permissions.
        -   Handles `DeniedAlwaysException` by opening app settings.
        - Handles `DeniedException` when permission is denied.
    - **Background permissions**:
        - Allows to grant background permissions.

---
+++ scan-and-connect-device/src/commonMain/kotlin/com/snc/device/presentation/DiscoverDevicesPageViewModel.kt
@@ -0,0 +1,159 @@
+package com.snc.device.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.platform.api.TargetPlatform
+import com.platform.api.getPlatform
+import com.platform.api.model.SwimConnectDevice
+import com.platform.api.util.BluetoothManager
+import com.snc.device.domain.RuntimePermissionsUseCase
+import com.snc.device.model.RuntimePermission
+import dev.icerock.moko.permissions.DeniedAlwaysException
+import dev.icerock.moko.permissions.DeniedException
+import dev.icerock.moko.permissions.PermissionState
+import dev.icerock.moko.permissions.PermissionsController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class DiscoverDevicesPageViewModel(
+    private val permissionController: PermissionsController
+) : ViewModel() {
+    private val _discoverDevicesPageUiState = MutableStateFlow(DiscoverDevicesPageUiState())
+    val discoverDevicesPageUiState = _discoverDevicesPageUiState.asStateFlow()
+
+    private val currentPlatform = getPlatform()
+
+    init {
+        if (currentPlatform == TargetPlatform.ANDROID) {
+            _discoverDevicesPageUiState.value = _discoverDevicesPageUiState.value.copy(
+                allPermissionsAreGranted = true,
+                isLocationEnabled = true
+            )
+        } else {
+            _discoverDevicesPageUiState.value = _discoverDevicesPageUiState.value.copy(
+                runtimePermissionsList = RuntimePermissionsUseCase.getRuntimePermissionsList()
+            )
+            checkIfAllPermissionsAreGranted()
+        }
+    }
+
+    fun checkIfAllPermissionsAreGranted() {
+        viewModelScope.launch {
+            updateRuntimePermissionListInternal()
+            val allGranted = areAllPermissionsGranted()
+            val mainGranted = areMainPermissionsGranted()
+
+            _discoverDevicesPageUiState.value = _discoverDevicesPageUiState.value.copy(
+                allPermissionsAreGranted = allGranted,
+                mainPermissionAreGranted = mainGranted
+            )
+        }
+    }
+
+    private suspend fun updateRuntimePermissionListInternal() {
+        _discoverDevicesPageUiState.value = _discoverDevicesPageUiState.value.copy(
+            runtimePermissionsList = discoverDevicesPageUiState.value.runtimePermissionsList.map {
+                it.copy(
+                    isPermissionGranted = permissionController.isPermissionGranted(it.permission)
+                )
+            }
+        )
+    }
+
+    private suspend fun areAllPermissionsGranted(): Boolean {
+        return _discoverDevicesPageUiState.value.runtimePermissionsList.all {
+            permissionController.getPermissionState(it.permission) !in listOf(
+                PermissionState.Denied,
+                PermissionState.DeniedAlways,
+                PermissionState.NotGranted,
+                PermissionState.NotDetermined
+            )
+        }
+    }
+
+    private suspend fun areMainPermissionsGranted(): Boolean {
+        return _discoverDevicesPageUiState.value.runtimePermissionsList
+            .filter { it.showOnUi }
+            .all { permissionController.isPermissionGranted(it.permission) }
+    }
+
+    fun requestRuntimePermission(runtimePermission: RuntimePermission) {
+        viewModelScope.launch {
+            try {
+                permissionController.providePermission(permission = runtimePermission.permission)
+            } catch (_: DeniedAlwaysException) {
+                permissionController.openAppSettings()
+            } catch (_: DeniedException) {
+
+            }
+            checkIfAllPermissionsAreGranted()
+        }
+    }
+
+    fun requestBackgroundPermissions() {
+        viewModelScope.launch {
+            _discoverDevicesPageUiState.value.runtimePermissionsList.forEach {
+                if (!it.showOnUi) {
+                    try {
+                        permissionController.providePermission(permission = it.permission)
+                    } catch (_: DeniedException) {
+                        TODO()
+                    } catch (_: DeniedAlwaysException) {
+                        TODO()
+                    }
+                }
+            }
+            checkIfAllPermissionsAreGranted()
+        }
+    }
+
+    fun updateScannedDevicesList(device: SwimConnectDevice) {
+        val deviceList = _discoverDevicesPageUiState.value.deviceList
+            .toMutableList()
+        deviceList.add(device)
+        _discoverDevicesPageUiState.value = _discoverDevicesPageUiState.value
+            .copy(deviceList = deviceList)
+    }
+
+    fun startScanning() {
+        BluetoothManager.startScanning(
+            onDiscoveryStarted = {
+                _discoverDevicesPageUiState.value =
+                    _discoverDevicesPageUiState.value.copy(isScanning = true)
+            },
+            onDeviceFound = {
+                val currentDevice = it
+                if (currentDevice !in _discoverDevicesPageUiState.value.deviceList) {
+                    updateScannedDevicesList(device = it)
+                }
+            },
+            onBluetoothDisabledError = {},
+            onLocationDisabledError = {}
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            delay(30_000)
+            stopScanning()
+        }
+    }
+
+    fun startRescanning() {
+        _discoverDevicesPageUiState.value =
+            _discoverDevicesPageUiState.value.copy(deviceList = emptyList())
+        startScanning()
+    }
+
+    fun stopScanning() {
+        BluetoothManager.stopScanning()
+        _discoverDevicesPageUiState.value =
+            _discoverDevicesPageUiState.value.copy(isScanning = false)
+    }
+
+    fun connect(device: SwimConnectDevice) {
+        BluetoothManager.connectDevice(device = device)
+    }
+}
